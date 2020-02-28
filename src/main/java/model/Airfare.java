0c9e2eb716ed018f22cc70329dba4c0ca5fc899f
Sven Thirion <sven.thirion@slalom.com>
2020-02-28 11:59:34 -0600

    Adding GSA models

---
+++ src/main/java/model/Airfare.java
@@ -0,0 +1,172 @@
+package model;
+
+public class Airfare {
+    private Integer ID;
+    private String AWARD_YEAR;
+    private String ORIGIN_AIRPORT_ABBREV;
+    private String DESTINATION_AIRPORT_ABBREV;
+    private String ORIGIN_CITY_NAME;
+    private String ORIGIN_STATE;
+    private String ORIGIN_COUNTRY;
+    private String DESTINATION_CITY_NAME;
+    private String DESTINATION_STATE;
+    private String DESTINATION_COUNTRY;
+    private String AIRLINE_ABBREV;
+    private String AWARDED_SERV;
+    private String PAX_COUNT;
+    private Integer YCA_FARE;
+    private Integer XCA_FARE;
+    private Integer BUSINESS_FARE;
+    private String ORIGIN_AIRPORT_LOCATION;
+    private String DESTINATION_AIRPORT_LOCATION;
+
+
+    // Getter Methods
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public String getAWARD_YEAR() {
+        return AWARD_YEAR;
+    }
+
+    public String getORIGIN_AIRPORT_ABBREV() {
+        return ORIGIN_AIRPORT_ABBREV;
+    }
+
+    public String getDESTINATION_AIRPORT_ABBREV() {
+        return DESTINATION_AIRPORT_ABBREV;
+    }
+
+    public String getORIGIN_CITY_NAME() {
+        return ORIGIN_CITY_NAME;
+    }
+
+    public String getORIGIN_STATE() {
+        return ORIGIN_STATE;
+    }
+
+    public String getORIGIN_COUNTRY() {
+        return ORIGIN_COUNTRY;
+    }
+
+    public String getDESTINATION_CITY_NAME() {
+        return DESTINATION_CITY_NAME;
+    }
+
+    public String getDESTINATION_STATE() {
+        return DESTINATION_STATE;
+    }
+
+    public String getDESTINATION_COUNTRY() {
+        return DESTINATION_COUNTRY;
+    }
+
+    public String getAIRLINE_ABBREV() {
+        return AIRLINE_ABBREV;
+    }
+
+    public String getAWARDED_SERV() {
+        return AWARDED_SERV;
+    }
+
+    public String getPAX_COUNT() {
+        return PAX_COUNT;
+    }
+
+    public Integer getYCA_FARE() {
+        return YCA_FARE;
+    }
+
+    public Integer getXCA_FARE() {
+        return XCA_FARE;
+    }
+
+    public Integer getBUSINESS_FARE() {
+        return BUSINESS_FARE;
+    }
+
+    public String getORIGIN_AIRPORT_LOCATION() {
+        return ORIGIN_AIRPORT_LOCATION;
+    }
+
+    public String getDESTINATION_AIRPORT_LOCATION() {
+        return DESTINATION_AIRPORT_LOCATION;
+    }
+
+
+    // Setter Methods
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public void setAWARD_YEAR(String AWARD_YEAR) {
+        this.AWARD_YEAR = AWARD_YEAR;
+    }
+
+    public void setORIGIN_AIRPORT_ABBREV(String ORIGIN_AIRPORT_ABBREV) {
+        this.ORIGIN_AIRPORT_ABBREV = ORIGIN_AIRPORT_ABBREV;
+    }
+
+    public void setDESTINATION_AIRPORT_ABBREV(String DESTINATION_AIRPORT_ABBREV) {
+        this.DESTINATION_AIRPORT_ABBREV = DESTINATION_AIRPORT_ABBREV;
+    }
+
+    public void setORIGIN_CITY_NAME(String ORIGIN_CITY_NAME) {
+        this.ORIGIN_CITY_NAME = ORIGIN_CITY_NAME;
+    }
+
+    public void setORIGIN_STATE(String ORIGIN_STATE) {
+        this.ORIGIN_STATE = ORIGIN_STATE;
+    }
+
+    public void setORIGIN_COUNTRY(String ORIGIN_COUNTRY) {
+        this.ORIGIN_COUNTRY = ORIGIN_COUNTRY;
+    }
+
+    public void setDESTINATION_CITY_NAME(String DESTINATION_CITY_NAME) {
+        this.DESTINATION_CITY_NAME = DESTINATION_CITY_NAME;
+    }
+
+    public void setDESTINATION_STATE(String DESTINATION_STATE) {
+        this.DESTINATION_STATE = DESTINATION_STATE;
+    }
+
+    public void setDESTINATION_COUNTRY(String DESTINATION_COUNTRY) {
+        this.DESTINATION_COUNTRY = DESTINATION_COUNTRY;
+    }
+
+    public void setAIRLINE_ABBREV(String AIRLINE_ABBREV) {
+        this.AIRLINE_ABBREV = AIRLINE_ABBREV;
+    }
+
+    public void setAWARDED_SERV(String AWARDED_SERV) {
+        this.AWARDED_SERV = AWARDED_SERV;
+    }
+
+    public void setPAX_COUNT(String PAX_COUNT) {
+        this.PAX_COUNT = PAX_COUNT;
+    }
+
+    public void setYCA_FARE(Integer YCA_FARE) {
+        this.YCA_FARE = YCA_FARE;
+    }
+
+    public void setXCA_FARE(Integer XCA_FARE) {
+        this.XCA_FARE = XCA_FARE;
+    }
+
+    public void setBUSINESS_FARE(Integer BUSINESS_FARE) {
+        this.BUSINESS_FARE = BUSINESS_FARE;
+    }
+
+    public void setORIGIN_AIRPORT_LOCATION(String ORIGIN_AIRPORT_LOCATION) {
+        this.ORIGIN_AIRPORT_LOCATION = ORIGIN_AIRPORT_LOCATION;
+    }
+
+    public void setDESTINATION_AIRPORT_LOCATION(String DESTINATION_AIRPORT_LOCATION) {
+        this.DESTINATION_AIRPORT_LOCATION = DESTINATION_AIRPORT_LOCATION;
+    }
+}
